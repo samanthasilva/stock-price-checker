6db9e1137bca3d91433e5fb45eef9d28a986f219
samanthasilva <samanthinhasilva@gmail.com>
2024-10-16 19:12:58 -0300

    Update tabs.js

--- public/tabs.js
+++ public/tabs.js
@@ -1,13 +1,10 @@
-// Função para alternar entre as abas
 document.querySelectorAll('.tabs .tab a').forEach(tab => {
     tab.addEventListener('click', event => {
         event.preventDefault();
 
-        // Remove a classe 'active' de todas as abas e conteúdo
         document.querySelectorAll('.tabs .tab a').forEach(a => a.classList.remove('active'));
         document.querySelectorAll('.tab-content').forEach(content => content.classList.remove('active'));
 
-        // Adiciona a classe 'active' à aba e ao conteúdo correspondente
         tab.classList.add('active');
         const contentId = tab.getAttribute('data-tab');
         document.getElementById(contentId).classList.add('active');
